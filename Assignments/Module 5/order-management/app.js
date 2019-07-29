fd7ba09a369cf2d73d5bfa0f5caec93d2cd01dd9
Mahesh Kokate <mahesh.kokate@adcolony.com>
2019-07-29 22:52:04 +0530

    Module 5: Order management assignment part 2.

--- Assignments/Module 5/order-management/app.js
+++ Assignments/Module 5/order-management/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const ejs = require('ejs');
 const mongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const dateComparitor = require('compare-dates');
+const sgMail = require('@sendgrid/mail');
 const port = process.env.port || 3200;
 const app = express();
 const mongoUrl= "mongodb://localhost:27017";
@@ -19,7 +21,7 @@ app.get('/', function(req, res){
 app.post('/submitOrder', function(req, res){
     mongoClient.connect(mongoUrl, { useNewUrlParser: true }, function(error, db){
         if(error) {
-            res.status(401).send('error while connecting..')
+            res.status(401).send('Error while connecting to database')
         }else{
             const database = db.db('edureka');
             var order = {name: req.body.name, address: req.body.address, email: req.body.email, product: req.body.product, date: new Date()};
@@ -30,6 +32,52 @@ app.post('/submitOrder', function(req, res){
         }
     })
 })
+
+app.get('/dashboard', function(req, res){
+    mongoClient.connect(mongoUrl, { useNewUrlParser: true }, function(error, db){
+        if (error){
+            res.status(401).send('Error while connecting to database');
+        }else{
+            const dbo = db.db('edureka');
+            dbo.collection('orders').find({}).toArray(function(error, data){
+                if (error) {
+                    res.status(401).send('Error while getting records from database');
+                }else{
+                    for(var i =0; i < data.length; i++){
+                        var order = data[i];
+                        order.status = getStatus(order.date);
+                    }
+                    res.render('dashboard', {orders: data});
+                }
+            })
+        }
+    })
+})
+
+app.get('/sendEmail/:emailId/:status', function(req, res){
+    var emailId = req.params.emailId;
+    var status = req.params.status;
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: emailId,
+        from: 'mahesh.kokate@talentica.com',
+        subject: 'Order Status',
+        text: `You order status is ${status}`,
+        html: '<strong>Thank You</strong>',
+    };
+    sgMail.send(msg);
+    res.send(`Sent an email to ${emailId}`);
+})
+
+function getStatus(orderDate){
+    var today = new Date();
+    if (dateComparitor.isBefore(today, orderDate, 2, 'day')){
+        return "Delivered"
+    }else if (dateComparitor.isBefore(today, orderDate, 1, 'day')){
+        return "Dispatched"
+    }
+    return "In Progress"
+}
 
 app.listen(port, function(error){
     if(error) throw error;
